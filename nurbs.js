ad5099ec2819adb09038fe2e648728a817d670c2
Alexander Meißner <AlexanderMeissner@gmx.net>
2019-05-08 09:25:30 +0200

    3D support, cleanups and bug fixes.

--- nurbs.js
+++ nurbs.js
@@ -1,15 +1,15 @@
-import {vec2, vec3} from './gl-matrix/index.js';
+import {vec3, vec4} from './gl-matrix/index.js';
 
 export default class NURBS {
-    static makeHomogenousCoordinates(x, y, weight) {
-        const homogenous = vec3.fromValues(x, y, 1.0);
-        vec3.scale(homogenous, homogenous, weight);
+    static makeHomogenousCoordinates(x, y, z, weight) {
+        const homogenous = vec4.fromValues(x, y, z, 1.0);
+        vec4.scale(homogenous, homogenous, weight);
         return homogenous;
     }
 
     static projectHomogenousCoordinates(homogenous) {
-        const projected = vec2.create();
-        vec2.scale(projected, homogenous, (homogenous[2] == 0.0) ? 1.0 : 1.0/homogenous[2]);
+        const projected = vec3.create();
+        vec3.scale(projected, homogenous, (homogenous[3] == 0.0) ? 1.0 : 1.0/homogenous[3]);
         return projected;
     }
 
@@ -51,7 +51,7 @@ export default class NURBS {
             else
                 high = mid;
         }
-        return low-1;
+        return Math.max(this.degree, Math.min(low-1, this.maxKnotIndex-1));
     }
 
     basisWeightFunction(parameter, knotIndex, degree) {
@@ -69,28 +69,28 @@ export default class NURBS {
     }
 
     homogenousCoordinatesAt(parameter) {
-        const result = vec3.create();
+        const result = vec4.create();
         for(let knotIndex = 0; knotIndex < this.maxKnotIndex; ++knotIndex)
-            vec3.scaleAndAdd(result, result, this.controlPoints[knotIndex], this.basisFunction(parameter, knotIndex));
+            vec4.scaleAndAdd(result, result, this.controlPoints[knotIndex], this.basisFunction(parameter, knotIndex));
         return result;
     }
 
     deBoor(parameter, knotIndex) {
         knotIndex = Math.max(this.degree, Math.min(knotIndex, this.controlPoints.length-1));
-        const d = this.controlPoints.slice(knotIndex-this.degree, knotIndex+1).map(controlPoint => vec3.clone(controlPoint));
+        const points = this.controlPoints.slice(knotIndex-this.degree, knotIndex+1).map(controlPoint => vec4.clone(controlPoint));
         for(let r = 0; r < this.degree; ++r)
-            for(let j = this.degree; j > r; --j)
-                vec3.lerp(d[j], d[j-1], d[j], this.basisWeightFunction(parameter, j+knotIndex-this.degree, this.degree-r));
-        return d[this.degree];
+            for(let j = 0; j < this.degree-r; ++j)
+                vec4.lerp(points[j], points[j], points[j+1], this.basisWeightFunction(parameter, knotIndex-this.degree+j+r+1, this.degree-r));
+        return points[0];
     }
 
     deBoorIntermediateSteps(parameter, knotIndex) {
-        const results = [this.controlPoints.slice(knotIndex-this.degree, knotIndex+1).map(controlPoint => vec3.clone(controlPoint))];
+        const results = [this.controlPoints.slice(knotIndex-this.degree, knotIndex+1).map(controlPoint => vec4.clone(controlPoint))];
         for(let r = 0; r < this.degree; ++r) {
             results.push([]);
-            for(let j = this.degree; j > r; --j) {
-                results[r+1][j] = vec3.create();
-                vec3.lerp(results[r+1][j], results[r][j-1], results[r][j], this.basisWeightFunction(parameter, j+knotIndex-this.degree, this.degree-r));
+            for(let j = 0; j < this.degree-r; ++j) {
+                results[r+1][j] = vec4.create();
+                vec4.lerp(results[r+1][j], results[r][j], results[r][j+1], this.basisWeightFunction(parameter, knotIndex-this.degree+j+r+1, this.degree-r));
             }
         }
         return results;
@@ -99,14 +99,13 @@ export default class NURBS {
     // Wolfgang Böhm Algorithm
     insertKnotAt(parameter) {
         let knotIndex = this.knotIndexAtParameter(parameter);
-        const knotSpan = parameter-this.cumulativeKnotSpans[knotIndex++],
-              newControlPoints = [];
-        for(let i = knotIndex-this.degree; i < knotIndex; ++i) {
-            const controlPoint = vec3.create();
-            vec3.lerp(controlPoint, this.controlPoints[i-1], this.controlPoints[i], (parameter-this.cumulativeKnotSpans[i])/(this.cumulativeKnotSpans[i+this.degree]-this.cumulativeKnotSpans[i]));
-            newControlPoints.push(controlPoint);
-        }
-        this.controlPoints.splice(knotIndex-this.degree, this.degree-1, ...newControlPoints);
+        const knotSpan = parameter-this.cumulativeKnotSpans[knotIndex],
+              points = this.controlPoints.slice(knotIndex-this.degree, knotIndex+1).map(controlPoint => vec4.clone(controlPoint));
+        for(let j = 0; j < this.degree; ++j)
+            vec4.lerp(points[j], points[j], points[j+1], this.basisWeightFunction(parameter, knotIndex-this.degree+j+1, this.degree));
+        points.length = this.degree;
+        ++knotIndex;
+        this.controlPoints.splice(knotIndex-this.degree, this.degree-1, ...points);
         this.knotSpans[knotIndex] -= knotSpan;
         this.knotSpans.splice(knotIndex, 0, knotSpan);
         this.updateKnotSpans();
